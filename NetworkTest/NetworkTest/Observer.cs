8db891e22fd74cb5d79b570e04abb3b3cdb6aa62
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-03-24 13:27:23 -0700

    Updated Observable, started changes to VWClient

--- NetworkTest/NetworkTest/Observer.cs
+++ NetworkTest/NetworkTest/Observer.cs
@@ -10,18 +10,6 @@ public abstract class Observer
     ///  Provider attribute and member variable.
     /// </summary>
     protected NetworkManager provider;
-    public NetworkManager Provider
-    {
-        set
-        {
-            provider = value;
-        }
-        get
-        {
-            // yikes..
-            return provider;
-        }
-    }
 
     public virtual void OnDownloadQueued(String url) 
     {
