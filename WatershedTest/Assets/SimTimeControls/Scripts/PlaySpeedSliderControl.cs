8704c4776a577af631ad3f8662ea7df0e0b9c50e
Thomas Rushton <trushton134@gmail.com>
2015-07-09 15:39:09 -0700

    Updated time slider speed selection

--- WatershedTest/Assets/SimTimeControls/Scripts/PlaySpeedSliderControl.cs
+++ WatershedTest/Assets/SimTimeControls/Scripts/PlaySpeedSliderControl.cs
@@ -7,56 +7,145 @@
  * from the NSF Idaho EPSCoR Program and by the National Science Foundation.
  * 
  */
-
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 namespace VTL.SimTimeControls
 {
-	public class PlaySpeedSliderControl : MonoBehaviour
-	{
-		public TimeSlider timeSlider;
+    [System.Serializable]
 
-	    private Slider speedControlSlider;
+    public class PlaySpeedSliderControl : MonoBehaviour
+    {
+        public TimeSlider timeSlider;
+        public GameObject scaleSlider;
+        public enum timeScales { HOURS, DAYS, WEEKS, MONTHS }
+        public timeScales currentScale;
+        private Slider speedControlSlider;
+        private Text speedText;
+        int HOUR_STEPS = 24;
+        int DAY_STEPS = 7, DAY_MULTIPLIER = 24;
+        int WEEK_STEPS = 4, WEEK_MULTIPLIER = 168;
+        int MONTH_STEPS = 12, MONTH_MULTIPLIER = 730;
 
-	    private Text speedText;
+        // Use this for initialization
+        void Start()
+        {
+            speedControlSlider = gameObject.GetComponent<Slider>();
 
-		// Use this for initialization
-		void Start () 
-	    {
-	        speedControlSlider = gameObject.GetComponent<Slider>();
+            foreach (Transform child in transform)
+            {
+                if (child.name == "Speed Text")
+                    speedText = child.gameObject.GetComponent<Text>();
+            }
+            setSpeedText();
 
-			foreach (Transform child in transform) 
-			{
-				if (child.name == "Speed Text")
-					speedText = child.gameObject.GetComponent<Text>();
-			}
-	        
 
-	        Invoke("OnValueChange", 1f/60f);
-		}
-		
-		// Update is called once per frame
-		void Update () 
-	    {
-		}
-	    
-	    public void OnValueChange()
-	    {
-	        OnValueChange(speedControlSlider.value);
-	    }
+            //Invoke("OnValueChange", 1f / 60f);
 
-	    public void OnValueChange(float value)
-	    {
-	        timeSlider.PlaySpeed = Mathf.Pow(2f, value) / 8f;
-	        float spd = timeSlider.PlaySpeed;
-	        if (spd >= 1)
-	            speedText.text = string.Format("1s:{0}h", (int)spd);
-	        else
-	            speedText.text = string.Format("1s:{0}m", (float)(spd*60f));
+            //float spd = timeSlider.PlaySpeed;
+            //if (spd >= 1)
+            //    speedText.text = string.Format("1s:{0:0.000}h", spd);
+            //else
+            //    speedText.text = string.Format("1s:{0:0.00}m", spd * 60f);
+        }
 
-	    }
+        // Update is called once per frame
+        void Update()
+        {
+        }
 
-	}
+        
+        public void OnValueChange()
+        {
+            int newSpeed = (int)speedControlSlider.value;
+
+            if (currentScale == timeScales.HOURS)
+            {
+                timeSlider.PlaySpeed = (newSpeed + 1);
+            }
+            else if (currentScale == timeScales.DAYS)
+            {
+                timeSlider.PlaySpeed = DAY_MULTIPLIER * (newSpeed + 1);
+            }
+            else if (currentScale == timeScales.WEEKS)
+            {
+                timeSlider.PlaySpeed = WEEK_MULTIPLIER * (newSpeed + 1);
+            }
+            else if (currentScale == timeScales.MONTHS)
+            {
+                //not an accurate month speed
+                timeSlider.PlaySpeed = MONTH_MULTIPLIER * (newSpeed + 1);
+            }
+
+            //if (speedSelectionInterval == SpeedSelectionInterval.Exponential)
+            //{
+            //    speedControlSlider.wholeNumbers = true;
+            //    timeSlider.PlaySpeed = ExponentialSpeedSecToHourBaseline * Mathf.Pow(2f, value) / 4f;
+            //}
+            //else
+            //{
+            //    speedControlSlider.wholeNumbers = false;
+            //    timeSlider.PlaySpeed = Mathf.Lerp(LinearSpeedMin, LinearSpeedMax, value / 8f);
+            //}
+            setSpeedText();
+        }
+
+        public void updateScale()
+        {
+            int val = (int)scaleSlider.GetComponent<Slider>().value;
+
+            speedControlSlider.value = 0;
+            switch (val)
+            {
+                case 0: 
+                    currentScale = timeScales.HOURS;
+                    setNewSliderMax(HOUR_STEPS-1);
+                    break;
+                case 1:
+                    currentScale = timeScales.DAYS;
+                    setNewSliderMax(DAY_STEPS-1);
+                    break;
+                case 2:
+                    currentScale = timeScales.WEEKS;
+                    setNewSliderMax(WEEK_STEPS-1);
+                    break;
+                case 3:
+                    currentScale = timeScales.MONTHS;
+                    setNewSliderMax(MONTH_STEPS-1);
+                    break;
+                default:
+                    break;
+            }
+            setSpeedText();
+        }
+
+        public void setNewSliderMax(int maxVal)
+        {
+            speedControlSlider.maxValue = maxVal;
+        }
+
+        public void setSpeedText()
+        {
+            float spd = timeSlider.PlaySpeed;
+            if (spd >= MONTH_MULTIPLIER)
+            {
+                speedText.text = string.Format("1s:{0:0}month(s)", spd / MONTH_MULTIPLIER);
+            }
+            else if (spd >= WEEK_MULTIPLIER)
+            {
+                speedText.text = string.Format("1s:{0:0}week(s)", spd / WEEK_MULTIPLIER);
+            }
+            else if (spd >= DAY_MULTIPLIER)
+            {
+                speedText.text = string.Format("1s:{0:0}day(s)", spd / DAY_MULTIPLIER);
+            }
+            else
+            {
+                speedText.text = string.Format("1s:{0:0}hour(s)", spd);
+            }
+        }
+        
+    }
 }
