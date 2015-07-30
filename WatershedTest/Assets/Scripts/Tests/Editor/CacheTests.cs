14e88cd2c30a37963bbe4121fc5c0235f2519043
unknown <ccarthen@hpcvis5.rd.unr.edu>
2015-07-30 12:09:55 -0700

    Adding Cache tests

---
+++ WatershedTest/Assets/Scripts/Tests/Editor/CacheTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace CACHE_Test
+{
+	[TestFixture]
+	[Category("Cache Tests")]
+	internal class CacheTests
+	{
+		[Test]
+		public void CacheTest()
+		{
+			FileBasedCache.Insert<int> ("test", 1);
+			if (FileBasedCache.Exists ("test")) 
+			{
+				Debug.Log(FileBasedCache.Get<int>("test"));
+				Assert.AreEqual(1,FileBasedCache.Get<int>("test"));
+				return;
+			}
+			Assert.Fail ();
+		}
+	}
+}
