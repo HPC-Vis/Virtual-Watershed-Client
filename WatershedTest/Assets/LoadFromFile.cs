2c927683fe77ae0967e82359a2f7eb406671e7a7
Chase Carthen <chasec2331@gmail.com>
2015-09-03 18:19:51 -0700

    Pushing commit with fake fill in fields for the netcdf... with gdal

--- WatershedTest/Assets/LoadFromFile.cs
+++ WatershedTest/Assets/LoadFromFile.cs
@@ -51,9 +51,15 @@ public class LoadFromFile : MonoBehaviour {
     public void ChangeDirectory()
     {
         var contents = fileView.GetSelectedRowContent();
-        if(contents.Count > 0)
-        fileBrowser.SetDirectory((string)contents[0][0]);
-        populateFileWindow();
+        if (contents.Count > 0)
+        {
+            if (contents[0][2].ToString().ToLower() == "directory")
+            {
+                fileBrowser.SetDirectory((string)contents[0][0]);
+                populateFileWindow();
+            }
+        }
+
     }
 
     public void LoadFile()
@@ -69,11 +75,15 @@ public class LoadFromFile : MonoBehaviour {
                 RasterDataset modelData = new RasterDataset(RasterDataset.GetGdalPath(fileBrowser.CurrentDirectory + @"\" + contents[0][0]));
                 if (modelData.Open())
                 {
+                    Debug.LogError("ITS ALIVE");
                     ModelRun mr = new ModelRun(contents[0][0].ToString(), Guid.NewGuid().ToString(), "");
+                    mr.Location = GlobalConfig.Location;
+                    
                     ModelRunManager.InsertModelRun(mr.ModelRunUUID, mr);
                     List<string> subSets = modelData.GetSubDatasets();
                     foreach (String str in subSets)
                     {
+                        Debug.LogError("PROCESSING THIS STR: " + str);
                         RasterDataset rd = new RasterDataset(str);
                         if(rd.Open())
                         {
@@ -82,12 +92,19 @@ public class LoadFromFile : MonoBehaviour {
                             rec.Data = rd.GetData();
                             rec.modelRunUUID = mr.ModelRunUUID;
                             rec.id = Guid.NewGuid().ToString();
-                            ModelRunManager.InsertDataRecord(rec, new List<DataRecord>() { rec });
+                            rec.location = GlobalConfig.Location;
+                            rec.Temporal = (rec.Data.Count > 1);
+                            rec.bbox = "1 2 2 1"; // test bbox for now
+                            rec.projection = "epsg:4326";
+                            rec.start = DateTime.MinValue;
+                            rec.end = DateTime.MaxValue;
+                            ModelRunManager.InsertDataRecord(rec, new List<DataRecord>() );
                         }
 
                     }
                     var startupruns = FileBasedCache.Get<Dictionary<string,ModelRun>>("startup");
                     startupruns.Add(mr.ModelRunUUID, mr);
+                    Debug.LogError("ADDED: " + mr.ModelRunUUID);
                     FileBasedCache.Insert<Dictionary<string, ModelRun>>("startup",startupruns);
                 }
             }
