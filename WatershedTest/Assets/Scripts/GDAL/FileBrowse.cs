10b7c7db42fd0b79a2f92db913bcdc0f14d9be41
Chase Carthen <chasec2331@gmail.com>
2015-09-14 00:19:30 -0700

    We can now browse sub drives

--- WatershedTest/Assets/Scripts/GDAL/FileBrowse.cs
+++ WatershedTest/Assets/Scripts/GDAL/FileBrowse.cs
@@ -15,7 +15,7 @@ public struct DirectoryStruct
 }
 public class FileBrowse : MonoBehaviour {
 
-    public string CurrentDirectory = ".";
+    public string CurrentDirectory = Path.GetFullPath(".");
     string relativepath = ".";
 	// Use this for initialization
 	void Start () {
@@ -28,43 +28,78 @@ public class FileBrowse : MonoBehaviour {
 
     public void SetDirectory(string Directory)
     {
-        CurrentDirectory = Path.GetFullPath(CurrentDirectory + @"\" + Directory);
+        if(CurrentDirectory == "")
+        {
+            CurrentDirectory = Directory;
+            return;
+        }
+       
+        string PathTo = Path.IsPathRooted(Directory) ? Directory : Path.GetFullPath(CurrentDirectory + @"\" + Directory);
+        string Root = Path.GetPathRoot(PathTo);
+        Debug.LogError("PATHTO: " + PathTo);
+        Debug.LogError("ROOT: " + Root);
+        if(Directory == ".." && CurrentDirectory == Root)
+        {
+            CurrentDirectory = "";
+        }
+        else
+        {
+            CurrentDirectory = PathTo;
+        }
+        
     }
 
     public List<DirectoryStruct> GetContents()
     {
+
         List<DirectoryStruct> Contents = new List<DirectoryStruct>();
-        foreach(var i in Directory.GetDirectories(CurrentDirectory))
+        if (CurrentDirectory != "")
         {
-            DirectoryStruct temp = new DirectoryStruct();
-            temp.Path = Path.GetFullPath(i);
-            temp.IsDirectory = true;
-            temp.filename = i.Replace(CurrentDirectory+@"\","");
-            temp.dateModified = File.GetLastWriteTime(temp.Path).ToString();
-            Contents.Add(temp);
-            
+            foreach (var i in Directory.GetDirectories(CurrentDirectory))
+            {
+                DirectoryStruct temp = new DirectoryStruct();
+                temp.Path = Path.GetFullPath(i);
+                temp.IsDirectory = true;
+                temp.filename = i.Replace(CurrentDirectory + @"\", "");
+                temp.dateModified = File.GetLastWriteTime(temp.Path).ToString();
+                Contents.Add(temp);
+
+            }
+
+            foreach (var i in Directory.GetFiles(CurrentDirectory, "*.nc"))
+            {
+                Debug.LogError(i);
+                DirectoryStruct temp = new DirectoryStruct();
+                temp.Path = Path.GetFullPath(i);
+                temp.IsDirectory = false;
+                temp.filename = Path.GetFileName(i);
+                temp.dateModified = File.GetLastWriteTime(temp.Path).ToString();
+                float size = (float)new FileInfo(temp.Path).Length / 1024;
+                if (size > 1000)
+                {
+                    size /= 1000;
+                    temp.bytes = size.ToString("F2") + " MB";
+                }
+                else
+                {
+                    temp.bytes = size.ToString("F2") + " KB";
+                }
+
+                Contents.Add(temp);
+            }
         }
-
-        foreach(var i in Directory.GetFiles(CurrentDirectory,"*.nc"))
+        else
         {
-            Debug.LogError(i);
-            DirectoryStruct temp = new DirectoryStruct();
-            temp.Path = Path.GetFullPath(i);
-            temp.IsDirectory = false;
-            temp.filename = Path.GetFileName(i);
-            temp.dateModified = File.GetLastWriteTime(temp.Path).ToString();
-            float size = (float)new FileInfo(temp.Path).Length / 1024;
-            if (size > 1000)
+            Debug.LogError("DISPLAY DRIVES");
+            foreach (var i in Directory.GetLogicalDrives())
             {
-                size /= 1000;
-                temp.bytes = size.ToString("F2") + " MB";
+                DirectoryStruct temp = new DirectoryStruct();
+                Debug.LogError(i);
+                temp.IsDirectory = true;
+                temp.Path = i;
+                temp.filename = i;
+                Contents.Add(temp);
             }
-            else
-            {
-                temp.bytes = size.ToString("F2") + " KB";
-            }
-
-            Contents.Add(temp);
         }
         return Contents;
     }
