14312a14177d5ece33b507e5f7fa9db91095ef09
Chase Carthen <chasec2331@gmail.com>
2015-09-17 12:04:55 -0700

    Adding the double listener

---
+++ WatershedTest/Assets/Scripts/Unity/DoubleListener.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VTL.ListView;
+public delegate void DoTheDouble(System.Guid abc);
+public class DoubleListener : MonoBehaviour 
+{
+    public ListViewManager LVM;
+    List<System.Guid> GUIDSelected = new List<System.Guid>();
+    
+    //public event DoTheDouble Doubleness;
+    public DoTheDouble Doubleness;
+    bool doubleDetected = false;
+    
+	// Use this for initialization
+    public void Start()
+    {
+        ListViewManager.SelectionChangeEvent += ChangeDetected;
+    }
+
+    public void ChangeDetected()
+    {
+        doubleDetected = false;
+        var Selected = LVM.Selected();
+        List<System.Guid> GUIDOn = new List<System.Guid>();
+        
+        while(Selected.MoveNext())
+        {
+            var guid = (System.Guid)Selected.Current;
+            GUIDOn.Add(guid);
+            if(GUIDSelected.Contains(guid))
+            {
+                GUIDSelected.Remove(guid);
+            }
+        }
+        foreach(var i in GUIDSelected)
+        {
+            if (LVM.IsSelectedOn(i))
+            {
+                if (Doubleness != null)
+                {
+                    Doubleness(i);
+                }
+                doubleDetected = true;
+            }
+            //Debug.LogError("WHOA MAN!!!");
+        }
+
+        GUIDSelected = GUIDOn;
+    }
+
+}
