8c533a6af7c928caf7adabf5504ea956fdd5af22
Chase Carthen <chasec2331@gmail.com>
2016-02-08 14:33:41 -0800

    Adding a new world object class

---
+++ VirtualWatershedClient/Assets/Scripts/UnityVisObjects/WorldObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WorldObject : MonoBehaviour
+{
+
+    public DataRecord record;
+
+    public abstract void saveData();
+
+    public abstract void moveObject();
+
+    public abstract void alterData();
+
+    public abstract void getData();
+}
